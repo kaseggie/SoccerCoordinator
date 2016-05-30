3bdcb86c9a93e2a0d6c6d758ed96c7c5af2b2434
Kavan Seggie <kseggie@gmail.com>
2016-05-30 18:06:47 +0200

    Ready for code review

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -8,114 +8,137 @@
 let player1: [String:Any] = ["playerName": "Joe Smith",
                              "heightInInches": 42,
                              "hasPlayedBefore": true,
-                             "guardiansNames": "Jim and Jan Smith"]
+                             "guardianNames": "Jim and Jan Smith"]
 
 let player2: [String:Any] = ["playerName": "Jill Tanner",
                              "heightInInches": 36,
                              "hasPlayedBefore": true,
-                             "guardiansNames" : "Clara Tanner"]
-
+                             "guardianNames" : "Clara Tanner"]
 
 let player3: [String:Any] = ["playerName": "Bill Bon",
                              "heightInInches": 43,
                              "hasPlayedBefore": true,
-                             "guardiansNames": "Sara and Jenny Bon"]
+                             "guardianNames": "Sara and Jenny Bon"]
 
 let player4: [String:Any] = ["playerName": "Eva Gordon",
                              "heightInInches": 45,
                              "hasPlayedBefore": false,
-                             "guardiansNames": "Wendy and Mike Gordon"]
+                             "guardianNames": "Wendy and Mike Gordon"]
 
 let player5: [String:Any] = ["playerName": "Matt Gill",
                              "heightInInches": 40,
                              "hasPlayedBefore": false,
-                             "guardiansNames": "Charles and Sylvia Gill"]
+                             "guardianNames": "Charles and Sylvia Gill"]
 
 let player6: [String:Any] = ["playerName": "Kimmy Stein",
                              "heightInInches": 41,
                              "hasPlayedBefore": false,
-                             "guardiansNames": "Bill and Hillary Stein"]
+                             "guardianNames": "Bill and Hillary Stein"]
 
 let player7: [String:Any] = ["playerName": "Sammy Adams",
                              "heightInInches": 45,
                              "hasPlayedBefore": false,
-                             "guardiansNames": "Jeff Adams"]
+                             "guardianNames": "Jeff Adams"]
 
 let player8: [String:Any] = ["playerName": "Karl Saygan",
                              "heightInInches": 42,
                              "hasPlayedBefore": true,
-                             "guardiansNames": "Heather Bledsoe"]
+                             "guardianNames": "Heather Bledsoe"]
 
 let player9: [String:Any] = ["playerName": "Suzane Greenberg",
                              "heightInInches": 44,
                              "hasPlayedBefore": true,
-                             "guardiansNames": "Henrietta Dumas"]
+                             "guardianNames": "Henrietta Dumas"]
 
 let player10: [String:Any] = ["playerName": "Sal Dali",
                               "heightInInches": 41,
                               "hasPlayedBefore": false,
-                              "guardiansNames": "Gala Dali"]
+                              "guardianNames": "Gala Dali"]
 
 let player11: [String:Any] = ["playerName": "Joe Kavalier",
                               "heightInInches": 39,
                               "hasPlayedBefore": false,
-                              "guardiansNames": "Sam and Elaine Kavalier"]
+                              "guardianNames": "Sam and Elaine Kavalier"]
 
 let player12: [String:Any] = ["playerName": "Ben Finkelstein",
                               "heightInInches": 44,
                               "hasPlayedBefore": false,
-                              "guardiansNames": "Aaron and Jill Finkelstein"]
+                              "guardianNames": "Aaron and Jill Finkelstein"]
 
 let player13: [String:Any] = ["playerName": "Diego Soto",
                               "heightInInches": 41,
                               "hasPlayedBefore": true,
-                              "guardiansNames": "Robin and Sarika Soto"]
+                              "guardianNames": "Robin and Sarika Soto"]
 
 let player14: [String:Any] = ["playerName": "Chloe Alaska",
                               "heightInInches": 47,
                               "hasPlayedBefore": false,
-                              "guardiansNames": "David and Jamie Alaska"]
+                              "guardianNames": "David and Jamie Alaska"]
 
 let player15: [String:Any] = ["playerName": "Arnold Willis",
                               "heightInInches": 43,
                               "hasPlayedBefore": false,
-                              "guardiansNames": "Claire Willis"]
+                              "guardianNames": "Claire Willis"]
 
 let player16: [String:Any] = ["playerName": "Phillip Helm",
                               "heightInInches": 44,
                               "hasPlayedBefore": true,
-                              "guardiansNames": "Thomas Helm and Eva Jones"]
+                              "guardianNames": "Thomas Helm and Eva Jones"]
 
 let player17: [String:Any] = ["playerName": "Les Clay",
                               "heightInInches": 42,
                               "hasPlayedBefore": true,
-                              "guardiansNames": "Wynonna Brown"]
+                              "guardianNames": "Wynonna Brown"]
 
 let player18: [String:Any] = ["playerName": "Herschel Krustofski",
                               "heightInInches": 45,
                               "hasPlayedBefore": true,
-                              "guardiansNames": "Hyman and Rachel Krustofski"]
+                              "guardianNames": "Hyman and Rachel Krustofski"]
 
 // Manually create an array the contains each player dictionary
 var teamAll: [[String:Any]] = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+var heightSortedTeamAll: [[String:Any]]
 
-// Delcare the three team arrays
+// Declare the three team arrays
 var teamSharks: [[String:Any]] = []
 var teamDragons: [[String:Any]] = []
 var teamRaptors: [[String:Any]] = []
 
-// Function to assign players to teams, based on experience
-func assignHasPlayedBefore (hasPlayed: Bool) {
-    var counter = 1
-    for player in teamAll {
-        if (player["hasPlayedBefore"] as? Bool) == hasPlayed {
-            switch (counter % 3) {
-            case 0: teamSharks.append(player)
-            case 2: teamDragons.append(player)
-            case 1: teamRaptors.append(player)
-            default: print("This should programatically never occur")
+// Function that sorts the player array by height
+heightSortedTeamAll = teamAll.sort() {
+    ($0 ["heightInInches"] as? Int) < ($1 ["heightInInches"] as? Int)
+}
+
+// Function to assign players to teams, based on experience. Two separate for loops have been used with differing case appendings. This combined with the sorted array 'averages' players on height too.
+func assignHasPlayedBefore () {
+    var counter1 = 1
+    var counter2 = 1
+    for player in heightSortedTeamAll {
+        if (player["hasPlayedBefore"] as? Bool) == true {
+            switch (counter1 % 6) {
+                case 1: teamSharks.append(player)
+                case 2: teamDragons.append(player)
+                case 3: teamRaptors.append(player)
+                case 4: teamRaptors.append(player)
+                case 5: teamDragons.append(player)
+                case 0: teamSharks.append(player)
+                default: print("This should programatically never occur")
             }
-            counter += 1
+            counter1 += 1
+        }
+    }
+    for player in heightSortedTeamAll {
+        if (player["hasPlayedBefore"] as? Bool) == false {
+            switch (counter2 % 6) {
+                case 1: teamRaptors.append(player)
+                case 2: teamDragons.append(player)
+                case 3: teamSharks.append(player)
+                case 4: teamSharks.append(player)
+                case 5: teamDragons.append(player)
+                case 0: teamRaptors.append(player)
+                default: print("This should programatically never occur")
+            }
+            counter2 += 1
         }
     }
 }
@@ -148,8 +171,7 @@ func calcAverageHeight(arrayReference: Int) -> (Double) {
     return averageHeight
 }
 
-assignHasPlayedBefore(true)
-assignHasPlayedBefore(false)
+assignHasPlayedBefore()
 
 calcAverageHeight(1)
 calcAverageHeight(2)
@@ -165,9 +187,8 @@ teamRaptors
 for player in teamSharks {
     var teamName = "Sharks"
     var practiceDetails = "March 17, 3pm"
-    // Added additional \n to provide the paragraph break, is there a better way to do this?
-    print("Hi \(player["guardiansNames"])\n")
-    print("In order to have the soccer teams that can compete against each other, we have decided to allocate children to a team based on their experience. Each team will have a similar number of both experienced and inexperienced soccer players.\n")
+    print("Hi \(player["guardianNames"])\n")
+    print("In order to have soccer teams that can compete against each other, we have decided to allocate children to a team based on their experience. Each team will have a similar number of both experienced and inexperienced soccer players.\n")
     print("\(player["playerName"]) has been selected to play on the \(teamName) team. The \(teamName) will have their first practice on \(practiceDetails).\n")
     print("Regards, \n\n Kavan Seggie \nLeague Coordinator")
 }
@@ -175,8 +196,8 @@ for player in teamSharks {
 for player in teamDragons {
     var teamName = "Dragons"
     var practiceDetails = "March 17, 1pm"
-    print("Hi \(player["guardiansNames"])\n")
-    print("In order to have the soccer teams that can compete against each other, we have decided to allocate children to a team based on their experience. Each team will have a similar number of both experienced and inexperienced soccer players.\n")
+    print("Hi \(player["guardianNames"])\n")
+    print("In order to have soccer teams that can compete against each other, we have decided to allocate children to a team based on their experience. Each team will have a similar number of both experienced and inexperienced soccer players.\n")
     print("\(player["playerName"]) has been selected to play on the \(teamName) team. The \(teamName) will have their first practice on \(practiceDetails).\n")
     print("Regards, \n\n Kavan Seggie \nLeague Coordinator")
 }
@@ -185,67 +206,9 @@ for player in teamDragons {
 for player in teamRaptors {
     var teamName = "Raptors"
     var practiceDetails = "March 18, 1pm"
-    print("Hi \(player["guardiansNames"])\n")
-    print("In order to have the soccer teams that can compete against each other, we have decided to allocate children to a team based on their experience. Each team will have a similar number of both experienced and inexperienced soccer players.\n")
+    print("Hi \(player["guardianNames"])\n")
+    print("In order to have soccer teams that can compete against each other, we have decided to allocate children to a team based on their experience. Each team will have a similar number of both experienced and inexperienced soccer players.\n")
     print("\(player["playerName"]) has been selected to play on the \(teamName) team. The \(teamName) will have their first practice on \(practiceDetails).\n")
     print("Regards, \n\n Kavan Seggie \nLeague Coordinator")
 }
-
-
-//// Calculating average height
-//var totalSharksHeight: Int = 0
-//var averageSharksHeight: Double = 0
-//
-//for player in teamSharks {
-//  totalSharksHeight += (player["heightInInches"] as? Int)!
-//}
-//
-//averageSharksHeight = Double(totalSharksHeight) / Double(teamSharks.count)
-//
-//// Calculating average height
-//var totalDragonsHeight: Int = 0
-//var averageDragonsHeight: Double = 0
-//
-//for player in teamDragons {
-//    totalDragonsHeight += (player["heightInInches"] as? Int)!
-//}
-//
-//averageDragonsHeight = Double(totalDragonsHeight) / Double(teamDragons.count)
-//
-//// Calculating average height
-//var totalRaptorsHeight: Int = 0
-//var averageRaptorsHeight: Double = 0
-//
-//for player in teamRaptors {
-//    totalRaptorsHeight += (player["heightInInches"] as? Int)!
-//}
-//
-//averageRaptorsHeight = Double(totalRaptorsHeight) / Double(teamRaptors.count)
-
-
-//// Loop through all players and assign those who have played before. The "as? Bool" unpacks the Boolean from the Any type in the Dict.
-//for player in teamAll {
-//    if (player["hasPlayedBefore"] as? Bool) == true {
-//        switch (counter % 3) {
-//            case 1: teamSharks.append(player)
-//            case 2: teamDragons.append(player)
-//            case 0: teamRaptors.append(player)
-//            default: print("This should programatically never occur")
-//        }
-//        counter += 1
-//    }
-//}
-//
-//// Loop through all players and assign those who have NOT played before.
-//for player in teamAll {
-//    if (player["hasPlayedBefore"] as? Bool) == false {
-//        switch (counter % 3) {
-//        case 0: teamSharks.append(player)
-//        case 1: teamDragons.append(player)
-//        case 2: teamRaptors.append(player)
-//        default: print("This should programatically never occur")
-//        }
-//        counter += 1
-//    }
-//}
 
